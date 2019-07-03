a1f25055ccdfed096a13aad58542d19bf64969c3
EMMANUEL OREOLUWA <awuloero13@gmail.com>
2019-07-03 11:52:56 +0100

    chat

--- src/components/ChatAvatar.js
+++ src/components/ChatAvatar.js
@@ -4,9 +4,30 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 export default class ChatAvatar extends Component<{}> {
     render() {
       return (
+          <View style={styles.container}>
       <View>
-         <Icon name="user-circle" size={50}/>
+         <Icon name="user-circle" size={50} color:'white'/>
+         </View>
+         <View style={styles.chatnameview}>
+         <Text style={styles.chatname}>
+         GUY DANGEROUS
+         </Text>
+         </View>
          </View>
  );
         }
         }
+        const styles = StyleSheet.create({
+            container:{
+                flexDirection: 'row',
+            },
+            chatname:{
+                fontSize:15,
+                fontWeight: 'bold',
+                color:'white'
+            },
+            chatnameview:{
+                marginTop: 15,
+                marginLeft: 10,
+            }
+        })
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -2,14 +2,15 @@ import React, {Component} from 'react';
 import {StyleSheet, View, StatusBar,Text} from 'react-native';
 import {Header, Left, Right,Button} from "native-base";
 import BackIcon from '../icons/BackIcon'
+import ChatAvatar from '../components/ChatAvatar'
 export default class App extends Component<{}> {
     render() {
       return (
           <View>
           <Header style={{ backgroundColor: '#800000' }} androidStatusBarColor="#800000"> 
-           <Left>
+
           <BackIcon/>
-           </Left>
+          <ChatAvatar/>
            <View>
              <Text>
              </Text>
