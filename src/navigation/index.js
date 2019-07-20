02725be03c5199bf6ac51465c023dc9147499664
EMMANUEL OREOLUWA <awuloero13@gmail.com>
2019-07-20 02:15:09 +0100

    implenting react navigation

--- src/navigation/index.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react';
-import App from '../pages/Welcome';
-import { createAppContainer, createStackNavigator } from 'react-navigation';
-
-import Welcome from './pages/Welcome';
-import Routes from './src/Routes';
-import Homescreen from './src/Homescreen';
-AppRegistry.registerComponent('NavigateApp', () => App)
